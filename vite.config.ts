c6d88190e643adc65e281863f0f9acbb0c7725b5
Adam Mikó <a.miko@cra.cz>
2022-08-31 14:58:15 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        eslintPlugin({
+            eslintOptions: {
+                fix: true
+            }
+        }),
+        react(),
+    ],
     server: {
         port: 3000,
         host: true,
